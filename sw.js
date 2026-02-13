77d5c24737c11dbe76ceda20ddcc56600f0484f8
uzosiuk-dot <uzosiuk@gmail.com>
2026-02-13 07:47:30 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lactate-coach-v2026-02-13-2";
+const CACHE_NAME = "lactate-coach-v2026-02-13-3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
 
 self.addEventListener("install", (event) => {
